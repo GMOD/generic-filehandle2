aa57fe96eb59e5688862878af7538dc53f5fe596
Colin <colin.diesh@gmail.com>
2025-06-10 11:35:42 -0400

    Updates

--- src/browser.ts
+++ src/browser.ts
@@ -2,5 +2,20 @@ export * from './filehandle.ts'
 
 export { default as BlobFile } from './blobFile.ts'
 export { default as RemoteFile } from './remoteFile.ts'
+export class LocalFile {
+  readFile() {
+    throw new Error('unimplemented')
+  }
+  read() {
+    throw new Error('unimplemented')
+  }
+  stat() {
+    throw new Error('unimplemented')
+  }
+  close() {
+    throw new Error('unimplemented')
+  }
+  /* do nothing in browser */
+}
 
 export { type GenericFilehandle } from './filehandle.ts'
