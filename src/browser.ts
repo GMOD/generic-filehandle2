0423dd68638de9324ece3d6c913a2ee722eb9e80
Colin <colin.diesh@gmail.com>
2025-06-09 22:41:43 -0400

    Working next.js

---
+++ src/browser.ts
@@ -0,0 +1,6 @@
+export * from './filehandle.ts'
+
+export { default as BlobFile } from './blobFile.ts'
+export { default as RemoteFile } from './remoteFile.ts'
+
+export { type GenericFilehandle } from './filehandle.ts'
